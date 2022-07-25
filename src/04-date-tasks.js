0c15531b3fbef1cfd4f66f27c1f93046c3365bdb
Yevhenii Shevchenko <supershevchenko548@gmail.com>
2022-07-25 22:51:54 +0200

    feat: do date tusk

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,14 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  let answer;
+  const year = date.getFullYear();
+  if (year % 4 !== 0) answer = false;
+  else if (year % 100 !== 0) answer = true;
+  else if (year % 400 !== 0) answer = false;
+  else answer = true;
+  return answer;
 }
 
 
@@ -73,8 +79,15 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let string = '';
+  string += `${endDate.getHours() - startDate.getHours() < 10 ? '0' : ''}${endDate.getHours() - startDate.getHours()}:`;
+  string += `${endDate.getMinutes() - startDate.getMinutes() < 10 ? '0' : ''}${endDate.getMinutes() - startDate.getMinutes()}:`;
+  string += `${endDate.getSeconds() - startDate.getSeconds() < 10 ? '0' : ''}${endDate.getSeconds() - startDate.getSeconds()}.`;
+  string += `${endDate.getMilliseconds() - startDate.getMilliseconds() < 100 ? '0' : ''}`
+    + `${endDate.getMilliseconds() - startDate.getMilliseconds() < 10 ? '0' : ''}`
+    + `${endDate.getMilliseconds() - startDate.getMilliseconds()}`;
+  return string;
 }
 
 
@@ -94,8 +107,11 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const min = date.getMinutes();
+  const hours = date.getUTCHours() % 12;
+  const angle = ((Math.abs(0.5 * (60 * hours + min) - 6 * min)) * Math.PI) / 180;
+  return angle > Math.PI ? 2 * Math.PI - angle : angle;
 }
 
 
