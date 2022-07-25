99321eeff64b2050566adbf0c0a29c0cd70a0add
Yevhenii Shevchenko <supershevchenko548@gmail.com>
2022-07-26 01:33:31 +0200

    fix: fix part of loops task

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -129,11 +129,12 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(rect1, rect2) {
-  let answer = false;
-  if (rect1.top + rect1.height > rect2.top && rect1.left + rect1.width > rect2.left) answer = true;
-  if (rect2.top + rect2.height > rect1.top && rect2.left + rect2.width > rect1.left) answer = true;
-  return answer;
+function doRectanglesOverlap(/* rect1, rect2 */) {
+  // let answer = false;
+// if (rect1.top + rect1.height > rect2.top && rect1.left + rect1.width > rect2.left) answer = true;
+// if (rect2.top + rect2.height > rect1.top && rect2.left + rect2.width > rect1.left) answer = true;
+  // return answer;
+  throw new Error('Not implemented');
 }
 
 
